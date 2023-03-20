686618762a49debc09a20d615c3c3a7a364d1217
Igor Phllipe <igorphellipe.123@gmail.com>
2023-03-20 15:30:48 -0300

    Teste
    This reverts commit 2118f6c54a9b8a99adcece239880e114d5dcc7d4.

---
+++ controle-de-fluxo-IF.py
@@ -0,0 +1,13 @@
+idade = 35
+if idade >= 18:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
+
+velocidade = 60
+if velocidade < 40:
+    print('Velocidade baixa!')
+elif velocidade < 100:
+    print('Velocidade moderada')
+else:
+    print('Cuidado! velocidade está alta')
