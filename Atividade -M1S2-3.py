28c6bd7fb19d914d6610e97e2a33f9c4ccec0016
Igor Phllipe <igorphellipe.123@gmail.com>
2023-03-21 15:27:34 -0300

    Alteração de pasta

--- Atividade -M1S2-3.py
+++
@@ -1,5 +0,0 @@
-#Criando lista com nomes:
-nomes = ['Fernanda', 'Julia', 'Frederico', 'João']
-#Utilizando o laço for para pegar cada nome da lista e mostrar na sequencia da lista
-for nome in nomes:
-    print(f'Olá {nome}! tenha um ótimo dia!')
