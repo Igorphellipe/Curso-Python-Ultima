27c987fa965d2975e16e51969bf5e5de7108b160
Igor Phllipe <igorphellipe.123@gmail.com>
2023-03-21 15:26:58 -0300

    Atividades

---
+++ Atividades-Ultima/Atividade-de-calculo.py
@@ -0,0 +1,10 @@
+'''Crie um programa que receba dois números inteiros e depois mostre o resultado da soma e da multiplicação, de um número pelo outro.'''
+
+numero1 = int(input('Informe um valor: '))
+numero2 = int(input('Informe outro valor '))
+soma = numero1 + numero2
+multiplicação = numero1 * numero2
+
+print(f'A soma entre {numero1} é {numero2} e igual a {soma}')
+
+print(f'A multiplicação entre {numero1} é {numero2} e igual a {multiplicação}')
