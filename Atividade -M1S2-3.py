679be0e8c9ff90fb03077c139bf77665f91c08fb
Igor Phllipe <igorphellipe.123@gmail.com>
2023-03-21 15:24:29 -0300

    Create Atividade -M1S2-3.py

---
+++ Atividade -M1S2-3.py
@@ -0,0 +1,5 @@
+#Criando lista com nomes:
+nomes = ['Fernanda', 'Julia', 'Frederico', 'João']
+#Utilizando o laço for para pegar cada nome da lista e mostrar na sequencia da lista
+for nome in nomes:
+    print(f'Olá {nome}! tenha um ótimo dia!')
