62a7f7555a0652d824bd2ca477a4d84ff8cac56a
Igor Phllipe <igorphellipe.123@gmail.com>
2023-05-25 20:34:56 -0300

    Update ATIVIDADE-MODULO3-SEMANA2.py

--- Atividades-Ultima/ATIVIDADE-MODULO3-SEMANA2.py
+++ Atividades-Ultima/ATIVIDADE-MODULO3-SEMANA2.py
@@ -1,12 +1,13 @@
 #ATIVIDADE DE FUNÇÃO QUE ROTORNA O VALOR DO JUROS
 def decorator_imprimir(juros):
-    def valor(capital, taxa, tempo):
-        print(f'Capital: {capital}, Taxa: {taxa}, Tempo: {tempo}')
-        juros(capital, taxa, tempo)
+    def valor(*args):
+        print(f'Capital: {args[0]}, Taxa: {args[1]}, Tempo: {args[2]}')
+        juros(*args)
         print('fim')
+        return (juros_s)
     return valor
 @decorator_imprimir
 def juros_s(capital, taxa, tempo):
     return print(f'Resultado juros: {capital * (taxa / 100) * tempo:.2f}' )
 
-juros_s(1200, 4, 6)
+juros_s(1300, 4, 6)
